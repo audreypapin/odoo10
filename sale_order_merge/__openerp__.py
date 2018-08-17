d909fbe74510619a902bacc8fe94f38b706d7b1f
Odrey <audrey.papin@optimadsi.fr>
2018-08-17 15:16:02 +0200

    Add files via upload

---
+++ sale_order_merge/__openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# © 2010-2018 OPTIMA DSI - OPTIMA COM - OPTIMA FIBRE
+# @author: Audrey Papin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Fusionner des devis",
+    "summary": "Permets la fusion de devis d'un même partenaire avant confirmation",
+    "version": "10",
+    "category": "Sales Management",
+    "website": "https://www.optimadsi.fr",
+    "author": "Audrey Papin",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "sale_stock",
+    ],
+    "data": [
+        "views/sale_order.xml",
+        "views/sale_order_merge.xml",
+    ],
+    'price': 15.00,
+    'currency': 'EUR'
+}
